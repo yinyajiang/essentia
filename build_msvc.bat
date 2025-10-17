eff1c9c6d480352b2cc0d425e2b873723a6fbda1
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-10-17 14:24:43 +0800

    Update build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,6 +1,7 @@
 call %~dp0\packaging\build-dependencies-msvc.bat
-cd %~dp0
+cd /d %~dp0
 cmake -B build -DCMAKE_PREFIX_PATH=%~dp0\packaging\msvc
 set PATH=%PATH%;%~dp0\packaging\msvc\bin
 cmake --build build --config release
+cmake --install build --config release --prefix "%~dp0\build\installed"
 pause
