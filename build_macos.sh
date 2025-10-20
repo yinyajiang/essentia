c3c772eaaae50ac5dd1a4526a7a16672787aeaaa
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-10-20 14:28:42 +0800

    up

--- build_macos.sh
+++ build_macos.sh
@@ -2,4 +2,4 @@
 # brew install eigen libyaml fftw ffmpeg libsamplerate libtag chromaprint
 # brew install eigen libsamplerate
 cmake --preset macOS-Release
-cmake --build --preset build-macOS-Release
+cmake --build --preset build-macOS-Release --config release
