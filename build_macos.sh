5e82cbdf64d59daa204680bfc44410ac9f1c1c18
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-11-06 09:30:55 +0800

    up

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,11 @@
 
 # brew install eigen libyaml fftw ffmpeg libsamplerate libtag chromaprint
 # brew install eigen libsamplerate
-cmake --preset macOS-preset
-cmake --build --preset build-macOS --config release
+export MACOSX_DEPLOYMENT_TARGET=11.0
+if [ "$(uname -m)" == "arm64" ]; then
+    cmake --preset macOS-arm-preset
+    cmake --build --preset build-macOS-arm --config release
+else
+    cmake --preset macOS-preset
+    cmake --build --preset build-macOS --config release
+fi
