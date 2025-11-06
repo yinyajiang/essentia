c7fab89f1edca11cc78b9d96cfc0755baf681528
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-11-06 10:45:21 +0800

    Update build_macos.sh

--- build_macos.sh
+++ build_macos.sh
@@ -3,9 +3,11 @@
 # brew install eigen libsamplerate
 export MACOSX_DEPLOYMENT_TARGET=11.0
 if [ "$(uname -m)" == "arm64" ]; then
+    echo "Building for macOS arm64"
     cmake --preset macOS-arm-preset
     cmake --build --preset build-macOS-arm --config release
 else
+    echo "Building for macOS x86_64"
     cmake --preset macOS-preset
     cmake --build --preset build-macOS --config release
 fi
